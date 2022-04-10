2f6fdb5fdc6afcb053724c2f983f9bd1866aca88
avendeep <pradeepg63@gmail.com>
2022-04-10 20:27:32 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Working..!</h1>
+      <h1>Working FINE..!</h1>
     </div>
   );
 }
